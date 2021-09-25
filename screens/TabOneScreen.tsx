4846c7d4cc8532eaea8738acfcd91c068851dadb
Rokas <rookas.rudzenskas@gmail.com>
2021-09-25 11:05:00 +0300

    Rokas

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -4,30 +4,15 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import tw from "tailwind-react-native-classnames";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+const TabOneScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Youtube 🙅‍♂️</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View style={tw`flex flex-1 items-center justify-center`}>
+      <Text style={tw`font-bold text-lg`}>Youtube 🙅‍♂️</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+
+export default TabOneScreen;
