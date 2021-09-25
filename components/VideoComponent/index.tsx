edf7bf5704be8a102af097103295d19ce1a48299
Rokas <rookas.rudzenskas@gmail.com>
2021-09-25 12:36:07 +0300

    Rokas

--- components/VideoComponent/index.tsx
+++ components/VideoComponent/index.tsx
@@ -5,7 +5,9 @@ import tw from "tailwind-react-native-classnames";
 const VideoComponent = () => {
     return (
         <View style={tw``}>
-            <Image source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/thumbnails/thumbnail3.jpeg'}} style={{ width: 250, height: 250 }} />
+            <View style={tw``}>
+                <Image source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/thumbnails/thumbnail3.jpeg'}} style={{ width: 250, height: 250 }} />
+            </View>
         </View>
     );
 };
