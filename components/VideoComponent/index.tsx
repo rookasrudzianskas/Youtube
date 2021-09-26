90e46c107075bb29e2a9ef7991776aa2086cead1
Rokas <rookas.rudzenskas@gmail.com>
2021-09-26 15:37:33 +0300

    Rokas

--- components/VideoComponent/index.tsx
+++ components/VideoComponent/index.tsx
@@ -7,8 +7,8 @@ const VideoComponent = () => {
         <View style={tw``}>
             <View style={tw``}>
                 <Image source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/thumbnails/thumbnail3.jpeg'}} style={{ width: '100%', aspectRatio: 16/9 }} />
-                <View style={tw``}>
-                    <Text style={tw``}>14:53</Text>
+                <View style={tw`bg-black opacity-75 rounded-md h-5 w-12 flex items-center justify-center`}>
+                    <Text style={tw`text-white`}>14:53</Text>
                 </View>
             </View>
         </View>
