cb0882210c7e34472fbbc79c858d73589437fb30
Rokas <rookas.rudzenskas@gmail.com>
2021-09-27 14:49:13 +0300

    Rokas

--- components/VideoComment/index.tsx
+++ components/VideoComment/index.tsx
@@ -24,6 +24,7 @@ const VideoComment = ({comment}: VideoCommentProps) => {
         <View style={tw`flex flex-row items-center`}>
             <Image source={{uri: comment?.user?.image}} style={tw`w-10 h-10 rounded-full`} />
             <Text style={tw`text-gray-100 ml-2`}>{comment?.comment}</Text>
+            {/*<Text>Rokas</Text>*/}
         </View>
     )
 }
--- components/VideoComments/index.tsx
+++ components/VideoComments/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {View, Text, FlatList} from 'react-native'
 import { BottomSheetFlatList } from '@gorhom/bottom-sheet';
 import VideoComment from '../VideoComment';
 import comments from '../../assets/data/comments.json';
@@ -8,7 +8,7 @@ import tw from "tailwind-react-native-classnames";
 const VideoComments = () => {
     return (
         <View style={tw`bg-gray-800`}>
-            <BottomSheetFlatList
+            <FlatList
                 data={comments}
                 renderItem={({item}) => <VideoComment comment={item} />}
             />
