4bd30d6467735c09dd8a9fb7990a42f9bc067b94
Rokas <rookas.rudzenskas@gmail.com>
2021-09-27 10:53:50 +0300

    Rokas

--- components/VideoListItem/index.tsx
+++ components/VideoListItem/index.tsx
@@ -31,7 +31,7 @@ const VideoListItem = (props: VideoListItemProps) => {
                     <View>
                         <Image source={{ uri: video?.thumbnail}} style={{ width: '100%', aspectRatio: 16/9 }} />
                         <View style={tw`bg-black opacity-75 rounded-md h-5 w-12 flex items-center justify-center absolute right-2 bottom-4`}>
-                            <Text style={tw`text-white`}>{minutes}:{seconds}</Text>
+                            <Text style={tw`text-white`}>{minutes}:{seconds < 10 ? '0' : ''}{seconds}</Text>
                         </View>
                     </View>
 
