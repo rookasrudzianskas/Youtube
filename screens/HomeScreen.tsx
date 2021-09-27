e36804ba32f0c8ee1fb697fae10cf13c6b78e08f
Rokas <rookas.rudzenskas@gmail.com>
2021-09-27 10:16:50 +0300

    Rokas

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -16,7 +16,7 @@ const HomeScreen = () => {
             </View>
 
             <View style={tw``}>
-                <FlatList  data={videos} renderItem={({item}) => (
+                <FlatList showsVerticalScrollIndicator={false} data={videos} renderItem={({item}) => (
                     <VideoListItem key={item.id} item={item} />
                 )}/>
             </View>
