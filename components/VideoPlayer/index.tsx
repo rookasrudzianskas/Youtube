ab21b924bb14f684470d7f97fadb308d93040b7a
Rokas <rookas.rudzenskas@gmail.com>
2021-09-27 13:05:04 +0300

    Rokas

--- components/VideoPlayer/index.tsx
+++ components/VideoPlayer/index.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import {View, Text} from "react-native";
 import {Video} from "expo-av";
 
-const VideoPlayer = () => {
+interface VideoPlayerProps {
+    videoURI: string,
+    thumbnailURI: string,
+}
+
+const VideoPlayer = (props: VideoPlayerProps) => {
     return (
         <View>
             <Video
                 source={{ uri: 'http://foo/bar.mp3' }}
                 style={{width: '100%', aspectRatio: 16/9}}
-                // posterSource={{
-                //     uri: thumbnailURI,
-                // }}
+                posterSource={{
+                    uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/thumbnails/thumbnail1.jpeg',
+                }}
                 posterStyle={{
                     resizeMode: 'cover',
                 }}
-                usePoster={false}
+                usePoster={true}
                 useNativeControls
                 resizeMode="contain"
             />
