e8c6a5763efb3f7a6186a0840e4c3e10a0249342
Rokas <rookas.rudzenskas@gmail.com>
2021-09-27 11:08:24 +0300

    Rokas

---
+++ screens/VideoScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+const VideoScreen = () => {
+    return (
+        <View style={tw``}>
+
+        </View>
+    );
+};
+
+export default VideoScreen;
