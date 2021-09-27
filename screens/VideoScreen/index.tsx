2428246de9f6cae406d82472db2c7a43ff3e09ba
Rokas <rookas.rudzenskas@gmail.com>
2021-09-27 12:02:02 +0300

    Rokas

--- screens/VideoScreen/index.tsx
+++ screens/VideoScreen/index.tsx
@@ -24,91 +24,93 @@ const VideoScreen = () => {
                 <Image source={{uri: video?.thumbnail}} style={{width: '100%', aspectRatio: 16/9}} />
             </View>
 
-            <View style={tw`flex flex-col px-3`}>
-                <View style={tw`flex flex-row items-center mt-6`}>
-                    <Text style={tw`text-blue-500`}>{video?.tags}</Text>
-                </View>
+            <ScrollView>
+                <View style={tw`flex flex-col px-3`}>
+                    <View style={tw`flex flex-row items-center mt-6`}>
+                        <Text style={tw`text-blue-500`}>{video?.tags}</Text>
+                    </View>
 
-                <View style={tw`mt-3`}>
-                    <Text style={tw`text-lg text-gray-100`}>{video?.title}</Text>
-                </View>
+                    <View style={tw`mt-3`}>
+                        <Text style={tw`text-lg text-gray-100`}>{video?.title}</Text>
+                    </View>
 
-                <View style={tw`flex flex-row mt-3`}>
-                    <Text style={tw`text-gray-300`}>{viewsString}</Text>
-                    <Text style={tw`text-gray-300 ml-3`}>{video.createdAt}</Text>
-                </View>
+                    <View style={tw`flex flex-row mt-3`}>
+                        <Text style={tw`text-gray-300`}>{viewsString}</Text>
+                        <Text style={tw`text-gray-300 ml-3`}>{video.createdAt}</Text>
+                    </View>
 
-                <View style={tw`mt-3`}>
-                    <ScrollView horizontal showsHorizontalScrollIndicator={false} >
-                        <View style={tw`flex items-center`}>
-                            <AntDesign name="like1" size={33} color="lightgrey" />
-                            <Text style={tw`text-gray-200 mt-1`}>{video.likes}</Text>
-                        </View>
+                    <View style={tw`mt-3`}>
+                        <ScrollView horizontal showsHorizontalScrollIndicator={false} >
+                            <View style={tw`flex items-center`}>
+                                <AntDesign name="like1" size={33} color="lightgrey" />
+                                <Text style={tw`text-gray-200 mt-1`}>{video.likes}</Text>
+                            </View>
 
-                        <View style={tw`flex items-center mx-3`}>
-                            <AntDesign name="dislike2" size={33} color="lightgrey" />
-                            <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
-                        </View>
+                            <View style={tw`flex items-center mx-3`}>
+                                <AntDesign name="dislike2" size={33} color="lightgrey" />
+                                <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
+                            </View>
 
-                        <View style={tw`flex items-center mx-3`}>
-                            <AntDesign name="export" size={33} color="lightgrey" />
-                            <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
-                        </View>
+                            <View style={tw`flex items-center mx-3`}>
+                                <AntDesign name="export" size={33} color="lightgrey" />
+                                <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
+                            </View>
 
-                        <View style={tw`flex items-center mx-3`}>
-                            <AntDesign name="download" size={33} color="lightgrey" />
-                            <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
-                        </View>
+                            <View style={tw`flex items-center mx-3`}>
+                                <AntDesign name="download" size={33} color="lightgrey" />
+                                <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
+                            </View>
 
-                        <View style={tw`flex items-center mx-3`}>
-                            <AntDesign name="download" size={33} color="lightgrey" />
-                            <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
-                        </View>
+                            <View style={tw`flex items-center mx-3`}>
+                                <AntDesign name="download" size={33} color="lightgrey" />
+                                <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
+                            </View>
 
-                        <View style={tw`flex items-center mx-3`}>
-                            <AntDesign name="download" size={33} color="lightgrey" />
-                            <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
-                        </View>
+                            <View style={tw`flex items-center mx-3`}>
+                                <AntDesign name="download" size={33} color="lightgrey" />
+                                <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
+                            </View>
 
-                        <View style={tw`flex items-center mx-3`}>
-                            <AntDesign name="download" size={33} color="lightgrey" />
-                            <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
-                        </View>
-                    </ScrollView>
-                </View>
+                            <View style={tw`flex items-center mx-3`}>
+                                <AntDesign name="download" size={33} color="lightgrey" />
+                                <Text style={tw`text-gray-200 mt-1`}>{video.dislikes}</Text>
+                            </View>
+                        </ScrollView>
+                    </View>
 
-                <View style={tw`mt-3 border-t-2 border-gray-600  border-b-2 border-gray-600`}>
-                    <View style={tw`flex flex-row items-center my-2`}>
-                        <View style={tw``}>
-                            <Image source={{uri: video?.user?.image}} style={tw`w-10 h-10 rounded-full`} />
-                        </View>
-                        <View style={tw`flex flex-col ml-2 justify-center flex-1`}>
-                            <Text style={tw`text-gray-100 text-lg`}>{video.user.name}</Text>
-                            <Text style={tw`text-gray-400`}>{video?.user?.subscribers} subscribers</Text>
-                        </View>
+                    <View style={tw`mt-3 border-t-2 border-gray-600  border-b-2 border-gray-600`}>
+                        <View style={tw`flex flex-row items-center my-2`}>
+                            <View style={tw``}>
+                                <Image source={{uri: video?.user?.image}} style={tw`w-10 h-10 rounded-full`} />
+                            </View>
+                            <View style={tw`flex flex-col ml-2 justify-center flex-1`}>
+                                <Text style={tw`text-gray-100 text-lg`}>{video.user.name}</Text>
+                                <Text style={tw`text-gray-400`}>{video?.user?.subscribers} subscribers</Text>
+                            </View>
 
-                        <View style={tw`flex `}>
-                            <Text style={tw`text-lg text-red-500`}>SUBSCRIBE</Text>
+                            <View style={tw`flex `}>
+                                <Text style={tw`text-lg text-red-500`}>SUBSCRIBE</Text>
+                            </View>
                         </View>
                     </View>
-                </View>
 
 
-                <View style={tw`mt-4`}>
-                    <TouchableOpacity onPress={openComments} activeOpacity={0.8}>
-                        <View style={tw``}>
-                            <View style={tw`flex flex-row`}>
-                                <Text style={tw`text-gray-100 text-lg`}>Comments</Text>
-                                <Text style={tw`text-gray-400 text-lg ml-3`}>300</Text>
-                            </View>
+                    <View style={tw`mt-4`}>
+                        <TouchableOpacity onPress={openComments} activeOpacity={0.8}>
                             <View style={tw``}>
-                                {/*<VideoComment comment={comments[0]}/>*/}
+                                <View style={tw`flex flex-row`}>
+                                    <Text style={tw`text-gray-100 text-lg`}>Comments</Text>
+                                    <Text style={tw`text-gray-400 text-lg ml-3`}>300</Text>
+                                </View>
+                                <View style={tw``}>
+                                    {/*<VideoComment comment={comments[0]}/>*/}
+                                </View>
                             </View>
-                        </View>
-                    </TouchableOpacity>
-                </View>
+                        </TouchableOpacity>
+                    </View>
 
-            </View>
+                </View>
+            </ScrollView>
         </View>
     );
 };
