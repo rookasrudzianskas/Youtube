0ad15fce866cc50a32f7cb5cd039d0608b133142
Rokas <rookas.rudzenskas@gmail.com>
2021-09-28 12:44:36 +0300

    Roaks

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,22 +3,17 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
 import tw from "tailwind-react-native-classnames";
 import Header from "../components/Header";
 import SuggestedWatch from "../components/SuggestedWatch";
 
+// @ts-ignore
 const TabOneScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
   return (
-    <View style={tw`flex`}>
-      {/*  Header   */}
-        <View style={tw``}>
-            <Header />
-            <SuggestedWatch />
-        </View>
-
-        <View style={tw``}>
-
+    <View style={tw`flex mt-16 items-center bg-black`}>
+        <View style={tw`bg-black flex items-center`}>
+            <Text style={tw`text-2xl text-white`}>Nothing here</Text>
+            <Text style={tw`text-4xl text-green-500 mt-2 font-bold`}>404</Text>
         </View>
     </View>
   );
