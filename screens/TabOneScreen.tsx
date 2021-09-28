7f9b9ede57891436b948436f0a6e45dc57fb8e33
Rokas <rookas.rudzenskas@gmail.com>
2021-09-28 17:04:25 +0300

    Rokas

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import tw from "tailwind-react-native-classnames";
 import Header from "../components/Header";
 import SuggestedWatch from "../components/SuggestedWatch";
+import { Auth } from 'aws-amplify';
 
 // @ts-ignore
 const TabOneScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
@@ -14,6 +15,12 @@ const TabOneScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
         <View style={tw`bg-black flex items-center`}>
             <Text style={tw`text-2xl text-white`}>Nothing here</Text>
             <Text style={tw`text-4xl text-green-500 mt-2 font-bold`}>404</Text>
+
+            <TouchableOpacity activeOpacity={0.8} onPress={() => Auth.signOut()}>
+                <View style={tw`mt-10 px-10 py-2 rounded-md`}>
+                    <Text style={tw`text-lg font-bold`}>Sign out</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     </View>
   );
