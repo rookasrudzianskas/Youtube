3fbd4c748a47a00b54796f878ad2c3065a4a4a69
Rokas <rookas.rudzenskas@gmail.com>
2021-09-28 12:59:45 +0300

    Rokas

--- components/VideoListItem/index.tsx
+++ components/VideoListItem/index.tsx
@@ -37,7 +37,7 @@ const VideoListItem = (props: VideoListItemProps) => {
 
     const openVideoPage = () => {
        // @ts-ignore
-        navigation.navigate('VideoScreen');
+        navigation.navigate('VideoScreen', {id: video.id});
     }
 
     return (
