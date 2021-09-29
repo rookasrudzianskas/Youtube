bc72a6cebef72fad216680969b08eb1610bb9aee
Rokas <rookas.rudzenskas@gmail.com>
2021-09-29 17:05:45 +0300

    Rokas

--- screens/VideoScreen/index.tsx
+++ screens/VideoScreen/index.tsx
@@ -197,7 +197,7 @@ const VideoScreen = () => {
                 backgroundComponent={() => <View style={{backgroundColor: 'blue'}} />}
                 ref={commentsSheetRef} snapPoints={[0, '100%']} index={-1}>
                 <View style={tw`flex bg-gray-800 flex-1`}>
-                    {comments.length > 0 && <VideoComments comments={comments}/>}
+                    {!!comments.length && <VideoComments comments={comments}/>}
                 </View>
             </BottomSheet>
 
