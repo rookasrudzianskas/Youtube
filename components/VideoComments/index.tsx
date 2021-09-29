b695a0e173c8db59c4c5745c240ef5d90b8fcfe9
Rokas <rookas.rudzenskas@gmail.com>
2021-09-29 18:10:04 +0300

    Rokas

--- components/VideoComments/index.tsx
+++ components/VideoComments/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {View, Text, FlatList, TextInput} from 'react-native'
+import {View, Text, FlatList, TextInput, TouchableOpacity} from 'react-native'
 import { BottomSheetFlatList } from '@gorhom/bottom-sheet';
 import VideoComment from '../VideoComment';
 import tw from "tailwind-react-native-classnames";
@@ -19,7 +19,12 @@ const VideoComments = ({comments}) => {
                 renderItem={({item}) => <VideoComment comment={item} />}
             />
             <View style={tw`flex mx-3 absolute -bottom-36 w-96 rounded-full`}>
-                <TextInput value={newComment} onValueChange={setNewComment} style={tw`h-10 bg-white pl-2 rounded-lg`} placeholder={"Write a new comment"} />
+                <TextInput value={newComment} onChangeText={setNewComment} style={tw`h-10 bg-white pl-2 rounded-lg`} placeholder={"Write a new comment"} />
+                <TouchableOpacity activeOpacity={0.8}>
+                    <View style={tw`mt-4 bg-green-500 w-36 flex items-center py-2 rounded-lg`}>
+                        <Text style={tw`font-bold text-gray-100`}>Comment</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         </View>
     )
