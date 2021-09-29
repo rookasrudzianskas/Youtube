72139fcf58e175070fdf79df40bc5ecf6872b13c
Rokas <rookas.rudzenskas@gmail.com>
2021-09-29 18:14:25 +0300

    Rokas

--- components/VideoComments/index.tsx
+++ components/VideoComments/index.tsx
@@ -4,10 +4,22 @@ import { BottomSheetFlatList } from '@gorhom/bottom-sheet';
 import VideoComment from '../VideoComment';
 import tw from "tailwind-react-native-classnames";
 
+import {Comment} from '../../src/models';
+import { DataStore } from 'aws-amplify';
 // @ts-ignore
 const VideoComments = ({comments}) => {
 
     const [newComment, setNewComment] = useState('');
+    const sendComment = async () => {
+        await DataStore.save(new Comment({
+            comment: newComment,
+            likes: 0,
+            dislikes: 0,
+            replies: 0,
+        }));
+
+        setNewComment('');
+    }
 
     return (
         <View style={tw`bg-gray-800`}>
@@ -20,7 +32,7 @@ const VideoComments = ({comments}) => {
             />
             <View style={tw`flex mx-3 absolute -bottom-36 w-96 rounded-full`}>
                 <TextInput value={newComment} onChangeText={setNewComment} style={tw`h-10 bg-white pl-2 rounded-lg`} placeholder={"Write a new comment"} />
-                <TouchableOpacity activeOpacity={0.8}>
+                <TouchableOpacity onPress={sendComment} activeOpacity={0.8}>
                     <View style={tw`mt-4 bg-green-500 w-36 flex items-center py-2 rounded-lg`}>
                         <Text style={tw`font-bold text-gray-100`}>Comment</Text>
                     </View>
