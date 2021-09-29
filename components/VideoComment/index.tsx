2084aaa971e3f0fc21dccad1777003cf9a1932ba
Rokas <rookas.rudzenskas@gmail.com>
2021-09-29 17:02:25 +0300

    Rokas

--- components/VideoComment/index.tsx
+++ components/VideoComment/index.tsx
@@ -3,26 +3,16 @@ import {View, Text, Image} from 'react-native'
 import comments from '../../assets/data/comments.json';
 import video from "../../assets/data/video.json";
 import tw from "tailwind-react-native-classnames";
+import { Comment } from '../../src/models';
 
 interface VideoCommentProps {
-    comment: {
-        id: string,
-        createdAt: string,
-        comment: string,
-        likes: number,
-        dislikes: number,
-        replies: number,
-        user: {
-            name: string,
-            image: string,
-        }
-    }
+    comment: Comment;
 }
 
 const VideoComment = ({comment}: VideoCommentProps) => {
     return (
         <View style={tw`flex flex-row items-center mx-3 my-1 bg-gray-700 p-2 rounded-xl`}>
-            <Image source={{uri: comment?.user?.image}} style={tw`w-10 h-10 rounded-full`} />
+            <Image source={{uri: comment?.User?.image}} style={tw`w-10 h-10 rounded-full`} />
             <Text style={tw`text-gray-100 ml-2`}>{comment?.comment}</Text>
             {/*<Text>Rokas</Text>*/}
         </View>
