8088d9850ebc6fb39b6e073551c8579ce3fb1997
Rokas <rookas.rudzenskas@gmail.com>
2021-09-29 17:45:57 +0300

    Rokas

--- components/VideoComments/index.tsx
+++ components/VideoComments/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import {View, Text, FlatList} from 'react-native'
+import {View, Text, FlatList, TextInput} from 'react-native'
 import { BottomSheetFlatList } from '@gorhom/bottom-sheet';
 import VideoComment from '../VideoComment';
 import tw from "tailwind-react-native-classnames";
-import {Input} from "@mui/icons-material";
 
 // @ts-ignore
 const VideoComments = ({comments}) => {
@@ -17,7 +16,7 @@ const VideoComments = ({comments}) => {
                 renderItem={({item}) => <VideoComment comment={item} />}
             />
 
-            <Input style={tw`h-10 bg-white`} placeholder={"Write a new comment"} />
+            <TextInput style={tw`h-10 bg-white`} placeholder={"Write a new comment"} />
         </View>
     )
 }
