d54dff06003d6f6552b70f24b5a47665cc987e9b
Rokas <rookas.rudzenskas@gmail.com>
2021-10-02 14:20:23 +0300

    Rokas

--- components/VideoComment/index.tsx
+++ components/VideoComment/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {View, Text, Image} from 'react-native'
 import comments from '../../assets/data/comments.json';
 import video from "../../assets/data/video.json";
 import tw from "tailwind-react-native-classnames";
-import { Comment } from '../../src/models';
+import {Comment, User} from '../../src/models';
+import { DataStore } from 'aws-amplify';
 
 interface VideoCommentProps {
     comment: Comment;
 }
 
 const VideoComment = ({comment}: VideoCommentProps) => {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        // @ts-ignore
+        DataStore.query(User, comment.userID).then(setUser);
+    }, []);
     return (
         <View style={tw`flex flex-row items-center mx-3 my-1 bg-gray-700 p-2 rounded-xl`}>
             <Image source={{uri: comment?.User?.image}} style={tw`w-10 h-10 rounded-full`} />
-            <Text style={tw`text-gray-100 ml-2`}>{comment?.comment}</Text>
+            <Text style={tw`text-gray-100 ml-2`}>{comment?.comment || 'Something cool'}</Text>
             {/*<Text>Rokas</Text>*/}
         </View>
     )
