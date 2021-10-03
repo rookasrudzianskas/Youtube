64b26046eeca5f0ea04128bea651d0e02b2aba96
Rokas <rookas.rudzenskas@gmail.com>
2021-10-03 11:56:14 +0300

    Rokas

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -25,7 +25,6 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
 }
 
 // A root stack navigator is often used for displaying modals on top of all other content
-// Read more here: https://reactnavigation.org/docs/modal
 const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
