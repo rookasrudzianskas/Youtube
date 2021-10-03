14e03148075cc4c75960c30a5eb07fa86bb4aab9
Rokas <rookas.rudzenskas@gmail.com>
2021-10-03 19:31:08 +0300

    Rokas

--- screens/VideoUploadScreen/index.tsx
+++ screens/VideoUploadScreen/index.tsx
@@ -21,7 +21,7 @@ const VideoUploadScreen = () => {
 
     const pickVideo = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Videos,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
