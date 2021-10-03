bc20d12a2044c06038886b2bce2dc80f3d850b3d
Rokas <rookas.rudzenskas@gmail.com>
2021-10-03 19:18:34 +0300

    Rokas

--- screens/VideoUploadScreen/index.tsx
+++ screens/VideoUploadScreen/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {View} from "react-native";
+import {View, Text} from "react-native";
 import tw from "tailwind-react-native-classnames";
 
 const VideoUploadScreen = () => {
     return (
         <View style={tw``}>
-
+            <View style={tw`mt-10 flex items-center`}>
+                <Text style={tw`text-white text-lg`}>Upload Video</Text>
+            </View>
         </View>
     );
 };
